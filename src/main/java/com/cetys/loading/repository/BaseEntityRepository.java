7a2722e87fb66ed7e15dd9bab1714f7f72716432
clanie1 <danybarociov@gmail.com>
2025-03-19 14:18:19 -0700

    Deleted BaseEntityRepository.java

--- src/main/java/com/cetys/loading/repository/BaseEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cetys.loading.repository;
-
-import com.cetys.loading.model.BaseEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BaseEntityRepository extends JpaRepository<BaseEntity, Long> {
-}
