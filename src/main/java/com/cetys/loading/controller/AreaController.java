06b13286bfee472a7b39d97854084113a7bf2fea
el-shago <92878520+el-shago@users.noreply.github.com>
2025-03-21 23:52:33 -0700

    Implemented Area controller. Main functionality is ready, unit tests to be added.

--- src/main/java/com/cetys/loading/controller/AreaController.java
+++ src/main/java/com/cetys/loading/controller/AreaController.java
@@ -1,22 +1,45 @@
 package com.cetys.loading.controller;
 
-import com.cetys.loading.repository.AreaRepository;
+import com.cetys.loading.service.AreaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.cetys.loading.model.Area;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/area")
+@RequestMapping("/api/areas")
 public class AreaController {
     @Autowired
-    AreaRepository areaRepository;
+    AreaService areaService;
 
-    @GetMapping("/")
-    public Object[] getAllAreas() {
-        return areaRepository.findAll().toArray();
+    @GetMapping
+    public ResponseEntity<List<Area>> getAreaList() {
+        List<Area> areas = areaService.getAreaList();
+        return ResponseEntity.ok(areas);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Area> getAreaById(@PathVariable Long id) {
+        Area area = areaService.getAreaById(id);
+        if (area != null) {
+            return ResponseEntity.ok(area);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Area> createArea(@RequestBody Area area) {
+        Area createdArea = areaService.createArea(area);
+        return ResponseEntity.status(201).body(createdArea);
+    }
+
+    @PutMapping
+    public ResponseEntity<Area> updateArea(@PathVariable Long id, @RequestBody Area areaDetails) {
+        Area updatedArea = areaService.updateArea(id, areaDetails);
+        return updatedArea != null ? ResponseEntity.ok(updatedArea)
+                : ResponseEntity.notFound().build();
+    }
 }
