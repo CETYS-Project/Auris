8b5c18e4267d9f1f7127280cab17fcefba27b826
Deyanira Bravo <deyanira.bravo@accenture.com>
2025-04-08 13:39:14 -0700

    valores auth0

--- src/main/java/com/cetys/loading/config/SecurityConfig.java
+++ src/main/java/com/cetys/loading/config/SecurityConfig.java
@@ -1,27 +1,24 @@
 package com.cetys.loading.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/public/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt()
-            );
-        return http.build();
-    }
+    @Value("https://auris-identifier")
+    private String audience;
+
+    @Value("https://dev-aodesvgtpd08tn2z.us.auth0.com/")
+    private String issuer;
+
+    @Value("http://localhost:3000")
+    private String corsAllowedOrigins;
+
+    @Value("false")
+    boolean webSecurityDebug;
 }
