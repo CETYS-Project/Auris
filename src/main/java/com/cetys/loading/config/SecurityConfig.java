e73892b6977da45ec73ce99ee3662ad03f91555a
Deyanira Bravo <deyanira.bravo@accenture.com>
2025-04-08 04:55:10 -0700

    otros errores lol

--- src/main/java/com/cetys/loading/config/SecurityConfig.java
+++ src/main/java/com/cetys/loading/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.cetys.loading.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
