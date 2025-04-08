9ab12441b5ca5b93b8287a682e45fc4fe7e25551
Deyanira Bravo <deyanira.bravo@accenture.com>
2025-04-08 13:42:26 -0700

    jose

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.security:spring-security-oauth2-jose")
 }
 
 tasks.withType<Test> {
