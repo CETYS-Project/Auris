d1dbb8b2ff272256013d2602dcd407cd4935472c
Deyanira Bravo <deyanira.bravo@accenture.com>
2025-04-08 04:56:30 -0700

    jwt

--- src/main/java/com/cetys/loading/config/SecurityConfig.java
+++ src/main/java/com/cetys/loading/config/SecurityConfig.java
@@ -15,7 +15,9 @@ public class SecurityConfig {
                 .requestMatchers("/public/**").permitAll()
                 .anyRequest().authenticated()
             )
-
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt()
+            );
         return http.build();
     }
 }
